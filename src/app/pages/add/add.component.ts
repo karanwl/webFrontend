dcabcf546155fd85ed99f2d48c69857e3bd3b754
Raghuveer Pendyala <raghuveerkumar126@gmail.com>
2022-04-13 12:55:55 +0530

    answer survey

--- src/app/pages/add/add.component.ts
+++ src/app/pages/add/add.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { SurveyRepository } from 'src/app/model/survey.repository';
 import { FormGroup, FormControl } from '@angular/forms';
 import { Router } from "@angular/router";
+import { Surveys } from 'src/app/model/survey.model';
+import { ActivatedRoute } from '@angular/router';
+import { asLiteral } from '@angular/compiler/src/render3/view/util';
 
 
 @Component({
@@ -11,13 +14,38 @@ import { Router } from "@angular/router";
 })
 export class AddComponent implements OnInit {
   //formdata!: FormGroup;
-  formdata: any = {}
+  formdata: any = {};
+  answerformdata:any={};
   Title: any;
+  surveyObject:Surveys;
+  surveyRequestObject:Surveys;
+  requestType:String;
+  surveyId:String;
 
   constructor(private repository: SurveyRepository,
-              private router: Router) { }
+              private router: Router,
+              private route: ActivatedRoute) { }
   //formdata!: FormGroup;
   displayName = "";
+
+  get surveys(): Surveys{
+   
+    if(JSON.parse(JSON.stringify(localStorage.getItem('user')))!==null){
+      this.displayName=JSON.parse(JSON.parse(JSON.stringify(localStorage.getItem('user')))).displayName;
+    }
+     this.repository.getSurveys().forEach(survey => {
+
+      if(survey._id===this.surveyId){
+
+        this.surveyObject =  survey;
+      }
+
+      
+    });
+
+    return this.surveyObject;
+  }
+
   ngOnInit() {
     this.formdata = new FormGroup({
         Title: new FormControl(),
@@ -28,11 +56,27 @@ export class AddComponent implements OnInit {
         Question_3: new FormControl()
       });
 
-     
+      this.route.queryParams
+      .subscribe(params => {
+        console.log(params); // { category: "fiction" }
+        this.surveyId = params['surveyId'];
+        this.requestType = params['requestType'];
+       
+      }
+    );
+
+
+
+    this.answerformdata = new FormGroup({
+      Answer_1: new FormControl(),
+      Answer_2: new FormControl(),
+      Answer_3: new FormControl()
+    });
+
+
   }
 
   onClickSubmit(data: any){
-    console.log(data);
     this.Title = data.Title;
 
     if(JSON.parse(JSON.stringify(localStorage.getItem('user')))!==null){
@@ -44,4 +88,33 @@ export class AddComponent implements OnInit {
     this.router.navigate(['/home']);
   }
 
+
+  onSubmitAnswer(data: any){
+
+    this.surveyRequestObject =new Surveys();
+        this.surveyRequestObject['Title'] =this.surveyObject.Title;
+        this.surveyRequestObject['Description'] =this.surveyObject.Description;
+        this.surveyRequestObject['Date'] =this.surveyObject.Date;
+        this.surveyRequestObject['Question_1'] =this.surveyObject.Question_1;
+        this.surveyRequestObject['Question_2'] =this.surveyObject.Question_2;
+        this.surveyRequestObject['Question_3'] =this.surveyObject.Question_3;
+        if(JSON.parse(JSON.stringify(localStorage.getItem('user')))!==null){
+          this.surveyRequestObject['User']=JSON.parse(JSON.parse(JSON.stringify(localStorage.getItem('user')))).displayName;
+        }
+        
+        
+    //alert(data.Answer_1)    
+    this.surveyRequestObject['Answer_1'] =data.Answer_1;
+    this.surveyRequestObject['Answer_2'] = data.Answer_2;
+    this.surveyRequestObject['Answer_3']=data.Answer_3;
+
+    //alert(this.surveyRequestObject);
+
+    this.repository.postSurveys(this.surveyRequestObject);
+
+    this.router.navigate(['/home']);
+  }
+
 }
+
+

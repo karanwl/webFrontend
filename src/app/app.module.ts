c8b93cb3fd06a4a7df50cfe03b4062f6b14da5c1
krupa3910 <landofcanada@outlook.com>
2022-04-14 01:32:02 -0400

    add page ui fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -40,6 +40,7 @@ export function jwtTokenGetter()
     ReactiveFormsModule,
     FormsModule,
     ViewSurveyResultModule,
+    DataTablesModule,
     JwtModule.forRoot({
       config: {
         tokenGetter: jwtTokenGetter
